5965980b76dc7ba66100ebb871c9fd07c1d9d002
XiaoLFeng <lfengzeng@vip.qq.com>
2020-08-22 12:53:11 +0800

    Site updated: 2020-08-22 12:53:03

--- js/autoload.js
+++ js/autoload.js
@@ -27,12 +27,12 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		loadExternalResource(live2d_path + "/css/waifu.css", "css"),
+		loadExternalResource(live2d_path + "/js/live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "/js/waifu-tips.js", "js")
 	]).then(() => {
 		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
+			waifuPath: live2d_path + "/js/waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "https://live2d.xiaolfeng.top/"
 		});
