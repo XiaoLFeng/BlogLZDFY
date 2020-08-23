85e670ebd8b7eb8e0658653309f61bab5c5ce07a
XiaoLFeng <lfengzeng@vip.qq.com>
2020-08-23 11:44:34 +0800

    Site updated: 2020-08-23 11:44:21

--- 301/kawashiros.php
+++ 301/kawashiros.php
@@ -1,7 +1,7 @@
 <?php
 $the_host = $_SERVER['HTTP_HOST'];
 $request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-if($the_host == 'www.xiaolfeng.cn')
+if($the_host == 'blog.kawashiros.club')
 {
 header('HTTP/1.1 301 Moved Permanently');
 header('Location: http://blog.kawashiros.club/'.$request_uri);//
